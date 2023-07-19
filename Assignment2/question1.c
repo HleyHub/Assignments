1c60f9efacfa3971a18db4cc7b3136f77a81735e
HleyHub <hashleyed@gmail.com>
2023-07-19 10:24:43 -0700

    Updated question 1

--- Assignment2/question1.c
+++ Assignment2/question1.c
@@ -4,47 +4,81 @@
 struct Date
 {
 	unsigned int day;
-	unsigned int year;
 	unsigned int month;
+	unsigned int year;
 };
 
 // A function to compare two "Date" structures. The result will return -1 if
 // date2 is more recent, 0 if they are the same, and  1 if date1 is more recent
-int compare_dates(struct Date date1, struct Date date2)
+int valid_date(const struct Date* date)
 {
-	if (date1.year > date2.year)
+	// Validation for day, month, and year value
+	if (date->month < 1 || date->month > 12)
+	{
+		return (0);
+	}
+	if (date->day < 1 || date->day > 31)
+	{
+		return (0);
+	}
+	if (date->month == 2)
+	{
+		// Check for leap year if it's February
+		if ((date->year % 4 == 0 && date->year % 100 != 0) || date->year % 400 == 0)
+		{
+			return (date->day <= 29);
+		}
+		else
+		{
+			return (date->day <= 28);
+		}
+	}
+	else if (date->month == 4 || date->month == 6 || date->month == 9 || date->month == 11)
+	{
+		return (date->day <= 30);
+	}
+	else
 	{
 		return (1);
 	}
-	else if (date1.year < date2.year)
+}
+
+int compare_dates(const struct Date* date1, const struct Date* date2)
+{
+	// Compare the years first
+	if (date1->year > date2->year)
+	{
+		return (1);
+	}
+	else if (date1->year < date2->year)
 	{
 		return (-1);
 	}
 	else
 	{
-		// If years are the same, check months
-		if (date1.month > date2.month)
+		// If years are equal, compare the months
+		if (date1->month > date2->month)
 		{
 			return (1);
 		}
-		else if (date1.month < date2.month)
+		else if (date1->month < date2->month)
 		{
 			return (-1);
 		}
 		else
 		{
-			// If months are the same, check days
-			if (date1.day > date2.day)
+			// If months are equal, compare the days
+			if (date1->day > date2->day)
 			{
 				return (1);
 			}
-			else if (date1.day < date2.day)
+			else if (date1->day < date2->day)
 			{
 				return (-1);
 			}
 			else
 			{
-				// If days are the same, the dates are the same
+				// If everything is equal, the dates are the same
 				return (0);
 			}
 		}
@@ -56,20 +90,28 @@ int main()
 	struct Date date1, date2;
 	int result;
 
-	date1 = printf("Please enter the first date (day month year): ");
-	scanf("%d %d %d", &date1.day, &date1.month, &date1.year);
+	// Input data for date1
+	printf("Please enter the first date (day month year): ");
+	scanf("%u %u %u", &date1.day, &date1.month, &date1.year);
 
-	date2 = printf("Please enter the second date (day month year): ");
-	scanf("%d %d %d", &date2.day, &date2.month, &date2.year);
+	// Input data for date2
+	printf("Please enter the second date (day month year): ");
+	scanf("%u %u %u", &date2.day, &date2.month, &date2.year);
 
-	result = compare_dates(date1, date2);
-	if (result == 1)
+	// Check if the input dates are valid
+	if (!valid_date(&date1) || !valid_date(&date2))
 	{
-		printf("date1 is recent than date2.\n");
+		printf("This date is invalid!\n");
+		return (1);
 	}
-	else if (result == -1)
+	result = compare_dates(&date1, &date2);
+	if (result > 0)
 	{
-		printf("date2 is recent than date1.\n");
+		printf("date1 is recent than date2\n");
+	}
+	else if (result < 0)
+	{
+		printf("date2 is recent than date1\n");
 	}
 	else
 	{
